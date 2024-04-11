9c56f0c65ddb0bd895aae8122fca44ee6e9b51bd
Otshepagetse Mokoena <ST10108570@imconnect.edu.za>
2024-04-11 10:54:12 +0200

    Created a function to hold the ingredients, their measurements and the steps to cook in arrays; the function also outputs them.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+namespace Recipe
+{
+    public class RecipeHolder
+    {
+        public void Makerecipe(string[] ingredients, double[,] measurements, string[] steps)
+        {
+            //testing the function by itself
+            Console.WriteLine("Ingredients: ");
+                        for (int i = 0; i < ingredients.Length; i++)
+            {
+                double quantity = measurements[i, 0];
+                string unit = GetUnit((int)measurements[i, 1]);
+
+                Console.WriteLine($"{quantity}{unit} {ingredients[i]}");
+            }
+            Console.WriteLine("\nSteps: ");
+                        for (int i = 0;i < steps.Length; i++)
+            {
+                Console.WriteLine($"{i+1}.{steps[i]}");
+            }
+        }
+    }
+}
